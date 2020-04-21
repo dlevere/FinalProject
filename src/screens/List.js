985dae6ea98b84e7faa44886c3a6c04864dca2c8
dlevere <dlevere@dtcc.edu>
2020-04-20 21:19:05 -0400

    fourth commit

--- src/screens/List.js
+++ src/screens/List.js
@@ -1,12 +1,64 @@
-import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, ActivityIndicator, Linking, FlatList, TouchableOpacity, Image } from 'react-native';
 
 export default class List extends Component {
-  render() {
+
+constructor(props)
+{
+  super(props);
+  this.state={
+    isLoading: true,
+    dataSource: null,
+  }
+}
+
+// Grabs the data from the website
+componentDidMount(){
+  return fetch('https://finalproject-d3ed6.firebaseio.com/items.json')
+    .then ((response) => response.json())
+    .then ((responseJson) => {
+
+      this.setState({
+        isLoading: false,
+        dataSource: responseJson.results,
+      })
+    })
+
+    .catch((error) => {
+      console.log(error)
+    });
+}
+
+_renderItem = ({item}) => (
+  <Text>
+  {item.asset}
+  {item.model}
+  {item.serial}
+  </Text>    
+);
+
+render() {
+  if(this.state.isLoading){
+
     return (
       <View>
-        <Text>List</Text>
+        <ActivityIndicator />
+      </View>
+    )
+  } 
+  else {
+    return(
+      <View>
+        <Text style={styles.title}>Items Within Inventory</Text>
+        <FlatList
+        data={this.state.dataSource}
+        renderItem={this._renderItem}
+        keyExtractor={(item, index) => index}
+        />
       </View>
     );
-  }
+  }  
+}
+
+
 }
